77aef2cfbd75e238cd6132bd9ca251743743f496
zenon191 <ngocnghia19011999@gmail.com>
2019-12-31 10:02:19 +0700

    react-redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {Provider} from 'react-redux';
+import { createStore } from "redux";
 
+function rootRedux(){
+    
+}
+const store = createStore(rootRedux);
 ReactDOM.render(
-    <App />, document.getElementById("root"));
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById("root"));
