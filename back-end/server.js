1c2b56660ce89f351aec6eff06552cc992bb8eb2
zenon191 <ngocnghia19011999@gmail.com>
2019-12-31 10:38:28 +0700

    test

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
-const server = app.listen(5000, () => {
+const server = app.listen(8080, () => {
     console.log(`Express running -> PORT ${server.address().port}`);
 });
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/device_summary', (req, res) => {
+    res.send({
+        "IOS": 60,
+        "Android": 40
+    })
+})
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
+axios.get('http://localhost:8080/device_summary')
+    .then(res => {
+        console.log(res.data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
 export default class App extends React.Component {
+
     render() {
-        return ("");
+        return ("he");
     }
 }
