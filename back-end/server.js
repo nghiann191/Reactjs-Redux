986eeec05976662e99ed295e3361d3d5f716964b
zenon191 <ngocnghia19011999@gmail.com>
2019-12-31 09:09:48 +0700

    first commit

---
+++ back-end/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const server = app.listen(5000, () => {
+    console.log(`Express running -> PORT ${server.address().port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
