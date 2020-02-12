497c9c99467ad86704750dd0e5862e250ea83c32
zenon191 <ngocnghia19011999@gmail.com>
2020-02-12 10:47:52 +0700

    chinh sua folder LineChart su dung hook

--- front-end/src/components/LineChart/Device.js
+++ front-end/src/components/LineChart/Device.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import LineChart from './LineChart';
-import { getDataLineChart } from '../../actions';
-import { connect } from 'react-redux';
+import React, { useEffect } from "react";
+import LineChart from "./LineChart";
+import { getDataLineChart } from "../../actions";
+import { connect } from "react-redux";
 
-class Device extends React.Component{
-  componentDidMount(){
-    this.props.getDataLineChart()
-  }
-  render() {
-    return (
-      <div>
-        <h3>Device</h3>
-        <LineChart/>
-      </div>
-    );
-  }
-}
-const mapDispatchToProps = (dispatch) => {
-  return{
-    getDataLineChart: (startDate, endDate) => {dispatch(getDataLineChart(startDate, endDate))}
-  }
-}
+const Device = props => {
+  useEffect(() => {
+    props.getDataLineChart();
+  });
+  return (
+    <div>
+      <h3>Device</h3>
+      <LineChart />
+    </div>
+  );
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    getDataLineChart: (startDate, endDate) => {
+      dispatch(getDataLineChart(startDate, endDate));
+    }
+  };
+};
 export default connect(null, mapDispatchToProps)(Device);
--- front-end/src/components/LineChart/LineChart.js
+++ front-end/src/components/LineChart/LineChart.js
@@ -10,69 +10,88 @@ import {
 import _ from "lodash";
 import moment from "moment";
 import { Button } from "reactstrap";
-import LineChartSpinner from '../Spinners/LineChartSpinner';
+import LineChartSpinner from "../Spinners/LineChartSpinner";
 
 const LineChart = props => {
   const [state, setState] = useState("day");
   let dataLabel;
   const data = props.getDataLineChart.getDataLineChart;
-  if (state === "day") {
-    dataLabel = _.map(data, (item, index) => ({
-      name: item.name,
-      data: _.map(data[index].data, b => ({
-        x: moment(b.x).format("LL"),
-        y: b.y
-      }))
-    }));
-  } else if (state === "week") {
-    dataLabel = _.map(data, (item, index) => ({
-      name: item.name,
-      data: _.map(
-        _.uniqBy(item.data, e => moment(e.x).format("GGGG WW")),
-        b => ({
-          x: moment(b.x).format("GGGG WW"),
-          y: _.reduce(
-            _.filter(
-              data[index].data,
-              c =>
-                moment(c.x).format("GGGG WW") === moment(b.x).format("GGGG WW")
-            ),
-            (total, item) => total + item.y,
-            0
-          )
-        })
-      )
-    }));
-  } else if (state === "month") {
-    dataLabel = _.map(data, (item, index) => ({
-      name: item.name,
-      data: _.map(
-        _.uniqBy(item.data, a => moment(a.x).format("M Y")),
-        b => ({
-          x: moment(b.x).format("MMMM YYYY"),
-          y: _.reduce(
-            _.filter(
-              data[index].data,
-              c => moment(c.x).format("M Y") === moment(b.x).format("M Y")
-            ),
-            (total, item) => total + item.y,
-            0
-          )
-        })
-      )
-    }));
+  switch (state) {
+    case "day": {
+      dataLabel = _.map(data, (item, index) => ({
+        name: item.name,
+        data: _.map(data[index].data, b => ({
+          x: moment(b.x).format("LL"),
+          y: b.y
+        }))
+      }));
+      break;
+    }
+    case "week": {
+      dataLabel = _.map(data, (item, index) => ({
+        name: item.name,
+        data: _.map(
+          _.uniqBy(item.data, e => moment(e.x).format("GGGG WW")),
+          b => ({
+            x: moment(b.x).format("GGGG WW"),
+            y: _.reduce(
+              _.filter(
+                data[index].data,
+                c =>
+                  moment(c.x).format("GGGG WW") ===
+                  moment(b.x).format("GGGG WW")
+              ),
+              (total, item) => total + item.y,
+              0
+            )
+          })
+        )
+      }));
+      break;
+    }
+    case "month": {
+      dataLabel = _.map(data, (item, index) => ({
+        name: item.name,
+        data: _.map(
+          _.uniqBy(item.data, a => moment(a.x).format("M Y")),
+          b => ({
+            x: moment(b.x).format("MMMM YYYY"),
+            y: _.reduce(
+              _.filter(
+                data[index].data,
+                c => moment(c.x).format("M Y") === moment(b.x).format("M Y")
+              ),
+              (total, item) => total + item.y,
+              0
+            )
+          })
+        )
+      }));
+      break;
+    }
+    default: {
+      
+    }
   }
 
   const color = ["#8e3ace", "#32aeca"];
   const status = props.getDataLineChart.isLoadingLine;
   return (
     <div>
-      <div style={{float:'right'}}>
-        <Button color="link" onClick={() => setState("day")}>Day</Button>
-        <Button color="link" onClick={() => setState("week")}>Week</Button>
-        <Button color="link" onClick={() => setState("month")}>Month</Button>
+      <div style={{ float: "right" }}>
+        <Button color="link" onClick={() => setState("day")}>
+          Day
+        </Button>
+        <Button color="link" onClick={() => setState("week")}>
+          Week
+        </Button>
+        <Button color="link" onClick={() => setState("month")}>
+          Month
+        </Button>
       </div>
-      {status ? <LineChartSpinner/> : (
+      {status ? (
+        <LineChartSpinner />
+      ) : (
         <div>
           <VictoryChart width={1000} height={470} scale={{ x: "time" }}>
             {state === "day" ? (
